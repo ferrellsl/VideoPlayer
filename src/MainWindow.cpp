079b343c7aa1adc4e085457651e0262ca5c85cea
Stephen Ferrell <sferrellblue@yahoo.com>
2021-11-24 21:09:19 -0700

    Add files via upload

---
+++ src/MainWindow.cpp
@@ -0,0 +1,387 @@
+﻿/**
+ * 叶海辉
+ * QQ群121376426
+ * http://blog.yundiantech.com/
+ */
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+#include <QPainter>
+#include <QPaintEvent>
+#include <QFileDialog>
+#include <QDebug>
+#include <QDesktopWidget>
+#include <QFontDatabase>
+#include <QMouseEvent>
+#include <QMessageBox>
+
+#include "AppConfig.h"
+#include "Base/FunctionTransfer.h"
+
+#include "Widget/SetVideoUrlDialog.h"
+
+Q_DECLARE_METATYPE(VideoPlayerState)
+
+MainWindow::MainWindow(QWidget *parent) :
+    DragAbleWidget(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this->getContainWidget());
+
+    FunctionTransfer::init(QThread::currentThreadId());
+
+    ///初始化播放器
+    VideoPlayer::initPlayer();
+
+    setWindowFlags(Qt::FramelessWindowHint);//|Qt::WindowStaysOnTopHint);  //使窗口的标题栏隐藏
+//    setAttribute(Qt::WA_TranslucentBackground);
+
+    //因为VideoPlayer::PlayerState是自定义的类型 要跨线程传递需要先注册一下
+    qRegisterMetaType<VideoPlayerState>();
+
+    connect(ui->pushButton_open, &QPushButton::clicked, this, &MainWindow::slotBtnClick);
+    connect(ui->toolButton_open, &QPushButton::clicked, this, &MainWindow::slotBtnClick);
+    connect(ui->pushButton_play, &QPushButton::clicked, this, &MainWindow::slotBtnClick);
+    connect(ui->pushButton_pause,&QPushButton::clicked, this, &MainWindow::slotBtnClick);
+    connect(ui->pushButton_stop, &QPushButton::clicked, this, &MainWindow::slotBtnClick);
+    connect(ui->pushButton_volume, &QPushButton::clicked, this, &MainWindow::slotBtnClick);
+
+    connect(ui->horizontalSlider, SIGNAL(sig_valueChanged(int)), this, SLOT(slotSliderMoved(int)));
+    connect(ui->horizontalSlider_volume, SIGNAL(valueChanged(int)), this, SLOT(slotSliderMoved(int)));
+
+    ui->page_video->setMouseTracking(true);
+    ui->widget_videoPlayer->setMouseTracking(true);
+//    ui->page_video->installEventFilter(this);
+    ui->widget_videoPlayer->installEventFilter(this);
+    ui->widget_container->installEventFilter(this);
+
+    mPlayer = new VideoPlayer();
+    mPlayer->setVideoPlayerCallBack(this);
+
+    mTimer = new QTimer; //定时器-获取当前视频时间
+    connect(mTimer, &QTimer::timeout, this, &MainWindow::slotTimerTimeOut);
+    mTimer->setInterval(500);
+
+    mTimer_CheckControlWidget = new QTimer; //用于控制控制界面的出现和隐藏
+    connect(mTimer_CheckControlWidget, &QTimer::timeout, this, &MainWindow::slotTimerTimeOut);
+    mTimer_CheckControlWidget->setInterval(2500);
+
+    mAnimation_ControlWidget  = new QPropertyAnimation(ui->widget_controller, "geometry");
+
+    ui->stackedWidget->setCurrentWidget(ui->page_open);
+    ui->pushButton_pause->hide();
+
+    resize(1024,768);
+    setTitle(QStringLiteral("My Player-V%1").arg(AppConfig::VERSION_NAME));
+
+    mVolume = mPlayer->getVolume();
+
+}
+
+MainWindow::~MainWindow()
+{
+qDebug()<<__FUNCTION__;
+
+    AppConfig::saveConfigInfoToFile();
+    AppConfig::removeDirectory(AppConfig::AppDataPath_Tmp);
+
+    delete ui;
+}
+
+void MainWindow::showOutControlWidget()
+{
+
+    mAnimation_ControlWidget->setDuration(800);
+
+    int w = ui->widget_controller->width();
+    int h = ui->widget_controller->height();
+    int x = 0;
+    int y = ui->widget_container->height() - ui->widget_controller->height();
+
+    if (ui->widget_controller->isHidden())
+    {
+        ui->widget_controller->show();
+        mAnimation_ControlWidget->setStartValue(ui->widget_controller->geometry());
+    }
+    else
+    {
+        mAnimation_ControlWidget->setStartValue(ui->widget_controller->geometry());
+    }
+
+//    mAnimation_ControlWidget->setKeyValueAt(0, QRect(0, 0, 00, 00));
+//    mAnimation_ControlWidget->setKeyValueAt(0.4, QRect(20, 250, 20, 30));
+//    mAnimation_ControlWidget->setKeyValueAt(0.8, QRect(100, 250, 20, 30));
+//    mAnimation_ControlWidget->setKeyValueAt(1, QRect(250, 250, 100, 30));
+    mAnimation_ControlWidget->setEndValue(QRect(x, y, w, h));
+    mAnimation_ControlWidget->setEasingCurve(QEasingCurve::Linear); //设置动画效果
+
+    mAnimation_ControlWidget->start();
+
+}
+
+void MainWindow::hideControlWidget()
+{
+    mAnimation_ControlWidget->setTargetObject(ui->widget_controller);
+
+    mAnimation_ControlWidget->setDuration(300);
+
+    int w = ui->widget_controller->width();
+    int h = ui->widget_controller->height();
+    int x = 0;
+    int y = ui->widget_container->height() + h;
+
+    mAnimation_ControlWidget->setStartValue(ui->widget_controller->geometry());
+    mAnimation_ControlWidget->setEndValue(QRect(x, y, w, h));
+    mAnimation_ControlWidget->setEasingCurve(QEasingCurve::Linear); //设置动画效果
+
+    mAnimation_ControlWidget->start();
+}
+
+
+void MainWindow::slotSliderMoved(int value)
+{
+    if (QObject::sender() == ui->horizontalSlider)
+    {
+        mPlayer->seek((qint64)value * 1000000);
+    }
+    else if (QObject::sender() == ui->horizontalSlider_volume)
+    {
+        mPlayer->setVolume(value / 100.0);
+        ui->label_volume->setText(QString("%1").arg(value));
+    }
+}
+
+void MainWindow::slotTimerTimeOut()
+{
+    if (QObject::sender() == mTimer)
+    {
+        qint64 Sec = mPlayer->getCurrentTime();
+
+        ui->horizontalSlider->setValue(Sec);
+
+		QString curTime;
+		QString hStr = QString("0%1").arg(Sec / 3600);
+		QString mStr = QString("0%1").arg(Sec / 60 % 60);
+		QString sStr = QString("0%1").arg(Sec % 60);
+		if (hStr == "00")
+		{
+			curTime = QString("%1:%2").arg(mStr.right(2)).arg(sStr.right(2));
+		}
+		else
+		{
+			curTime = QString("%1:%2:%3").arg(hStr).arg(mStr.right(2)).arg(sStr.right(2));
+		}
+
+        ui->label_currenttime->setText(curTime);
+    }
+    else if (QObject::sender() == mTimer_CheckControlWidget)
+    {
+        mTimer_CheckControlWidget->stop();
+        hideControlWidget();
+    }
+}
+
+void MainWindow::slotBtnClick(bool isChecked)
+{
+    if (QObject::sender() == ui->pushButton_play)
+    {
+        mPlayer->play();
+    }
+    else if (QObject::sender() == ui->pushButton_pause)
+    {
+        mPlayer->pause();
+    }
+    else if (QObject::sender() == ui->pushButton_stop)
+    {
+        mPlayer->stop(true);
+    }
+    else if (QObject::sender() == ui->pushButton_open || QObject::sender() == ui->toolButton_open)
+    {
+        SetVideoUrlDialog dialog;
+
+        dialog.setVideoUrl(AppConfig::gVideoFilePath);
+
+        if (dialog.exec() == QDialog::Accepted)
+        {
+            QString s = dialog.getVideoUrl();
+
+            if (!s.isEmpty())
+            {
+                mPlayer->stop(true); //如果在播放则先停止
+                mPlayer->startPlay(s.toStdString());
+
+                AppConfig::gVideoFilePath = s;
+                AppConfig::saveConfigInfoToFile();
+            }
+        }
+
+    }
+    else if (QObject::sender() == ui->pushButton_volume)
+    {
+       qDebug()<<isChecked;
+
+        bool isMute = isChecked;
+        mPlayer->setMute(isMute);
+
+        if (isMute)
+        {
+            mVolume = mPlayer->getVolume();
+
+            ui->horizontalSlider_volume->setValue(0);
+            ui->horizontalSlider_volume->setEnabled(false);
+            ui->label_volume->setText(QString("%1").arg(0));
+        }
+        else
+        {
+            int volume = mVolume * 100.0;
+            ui->horizontalSlider_volume->setValue(volume);
+            ui->horizontalSlider_volume->setEnabled(true);
+            ui->label_volume->setText(QString("%1").arg(volume));
+        }
+
+    }
+
+}
+
+///打开文件失败
+void MainWindow::onOpenVideoFileFailed(const int &code)
+{
+    FunctionTransfer::runInMainThread([=]()
+    {
+        QMessageBox::critical(NULL, "tips", QString("open file failed %1").arg(code));
+    });
+}
+
+///打开SDL失败的时候回调此函数
+void MainWindow::onOpenSdlFailed(const int &code)
+{
+    FunctionTransfer::runInMainThread([=]()
+    {
+        QMessageBox::critical(NULL, "tips", QString("open Sdl failed %1").arg(code));
+    });
+}
+
+///获取到视频时长的时候调用此函数
+void MainWindow::onTotalTimeChanged(const int64_t &uSec)
+{
+    FunctionTransfer::runInMainThread([=]()
+    {
+        qint64 Sec = uSec/1000000;
+
+        ui->horizontalSlider->setRange(0,Sec);
+
+		QString totalTime;
+		QString hStr = QString("0%1").arg(Sec/3600);
+		QString mStr = QString("0%1").arg(Sec / 60 % 60);
+		QString sStr = QString("0%1").arg(Sec % 60);
+		if (hStr == "00")
+		{
+			totalTime = QString("%1:%2").arg(mStr.right(2)).arg(sStr.right(2));
+		}
+		else
+		{
+			totalTime = QString("%1:%2:%3").arg(hStr).arg(mStr.right(2)).arg(sStr.right(2));
+		}
+
+        ui->label_totaltime->setText(totalTime);
+    });
+}
+
+///播放器状态改变的时候回调此函数
+void MainWindow::onPlayerStateChanged(const VideoPlayerState &state, const bool &hasVideo, const bool &hasAudio)
+{
+    FunctionTransfer::runInMainThread([=]()
+    {
+        if (state == VideoPlayer_Stop)
+        {
+            ui->stackedWidget->setCurrentWidget(ui->page_open);
+
+            ui->pushButton_pause->hide();
+            ui->widget_videoPlayer->clear();
+
+            ui->horizontalSlider->setValue(0);
+            ui->label_currenttime->setText("00:00");
+            ui->label_totaltime->setText("00:00");
+
+            mTimer->stop();
+
+        }
+        else if (state == VideoPlayer_Playing)
+        {
+            if (hasVideo)
+            {
+                ui->stackedWidget->setCurrentWidget(ui->page_video);
+            }
+            else
+            {
+                ui->stackedWidget->setCurrentWidget(ui->page_audio);
+            }
+
+            ui->pushButton_play->hide();
+            ui->pushButton_pause->show();
+
+            mTimer->start();
+        }
+        else if (state == VideoPlayer_Pause)
+        {
+            ui->pushButton_pause->hide();
+            ui->pushButton_play->show();
+        }
+    });
+}
+
+///显示视频数据，此函数不宜做耗时操作，否则会影响播放的流畅性。
+void MainWindow::onDisplayVideo(std::shared_ptr<VideoFrame> videoFrame)
+{
+    ui->widget_videoPlayer->inputOneFrame(videoFrame);
+}
+
+//图片显示部件时间过滤器处理
+bool MainWindow::eventFilter(QObject *target, QEvent *event)
+{
+    if(target == ui->widget_container)
+    {
+        if(event->type() == QEvent::Resize)
+        {
+            ///停止动画，防止此时刚好开始动画，导致位置出错
+            mAnimation_ControlWidget->stop();
+
+            QResizeEvent * e = (QResizeEvent*)event;
+            int w = e->size().width();
+            int h = e->size().height();
+            ui->stackedWidget->move(0, 0);
+            ui->stackedWidget->resize(w, h);
+
+            int x = 0;
+            int y = h - ui->widget_controller->height();
+            ui->widget_controller->move(x, y);
+            ui->widget_controller->resize(w, ui->widget_controller->height());
+        }
+    }
+    else if(target == ui->page_video || target == ui->widget_videoPlayer)
+    {
+        if(event->type() == QEvent::MouseMove || event->type() == QEvent::MouseButtonPress)
+        {
+            if (!mTimer_CheckControlWidget->isActive())
+            {
+                showOutControlWidget();
+            }
+
+            mTimer_CheckControlWidget->stop();
+            mTimer_CheckControlWidget->start();
+        }
+        else if(event->type() == QEvent::Enter)
+        {
+            showOutControlWidget();
+        }
+        else if(event->type() == QEvent::Leave)
+        {
+            mTimer_CheckControlWidget->stop();
+            mTimer_CheckControlWidget->start();
+        }
+    }
+
+    //其它部件产生的事件则交给基类处理
+    return DragAbleWidget::eventFilter(target, event);
+}
---
+++ src/MainWindow.h
@@ -0,0 +1,75 @@
+﻿/**
+ * 叶海辉
+ * QQ群121376426
+ * http://blog.yundiantech.com/
+ */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+
+#include <QImage>
+#include <QPaintEvent>
+#include <QTimer>
+#include <QPushButton>
+#include <QPropertyAnimation>
+
+#include "VideoPlayer/VideoPlayer.h"
+#include "DragAbleWidget.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+///这个是播放器的主界面 包括那些按钮和进度条之类的
+class MainWindow : public DragAbleWidget, public VideoPlayerCallBack
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    bool eventFilter(QObject *target, QEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+
+    VideoPlayer *mPlayer; //播放线程
+    QTimer *mTimer; //定时器-获取当前视频时间
+    float mVolume;
+
+    QTimer *mTimer_CheckControlWidget; //用于控制控制界面的出现和隐藏
+    QPropertyAnimation *mAnimation_ControlWidget;   //控制底部控制控件的出现和隐藏
+    void showOutControlWidget(); //显示底部控制控件
+    void hideControlWidget();    //隐藏底部控制控件
+
+private slots:
+    ///播放器相关的槽函数
+    void slotSliderMoved(int value);
+    void slotTimerTimeOut();
+    void slotBtnClick(bool isChecked);
+
+
+    ///以下函数，是播放器的回调函数，用于输出信息给界面
+protected:
+    ///打开文件失败
+    void onOpenVideoFileFailed(const int &code);
+
+    ///打开sdl失败的时候回调此函数
+    void onOpenSdlFailed(const int &code);
+
+    ///获取到视频时长的时候调用此函数
+    void onTotalTimeChanged(const int64_t &uSec);
+
+    ///播放器状态改变的时候回调此函数
+    void onPlayerStateChanged(const VideoPlayerState &state, const bool &hasVideo, const bool &hasAudio);
+
+    ///显示视频数据，此函数不宜做耗时操作，否则会影响播放的流畅性。
+    void onDisplayVideo(VideoFramePtr videoFrame);
+
+};
+
+#endif // MAINWINDOW_H
