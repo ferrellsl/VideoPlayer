8bc01153cb3e13855192772f9dca5ef540a037e4
Stephen Ferrell <sferrellblue@yahoo.com>
2021-11-24 21:45:43 -0700

    Add files via upload

---
+++ module/VideoPlayer/src/main.cpp
@@ -0,0 +1,23 @@
+﻿
+/**
+ * 叶海辉
+ * QQ群121376426
+ * http://blog.yundiantech.com/
+ */
+
+#include <QApplication>
+#include <QTextCodec>
+
+#include "VideoPlayer/VideoPlayer.h"
+
+#undef main
+int main(int argc, char *argv[])
+{
+    VideoPlayer *mPlayer = new VideoPlayer();
+//    mPlayer->setVideoPlayerCallBack(this);
+
+    mPlayer->startPlay("F:/test.rmvb");
+
+    return 0;
+}
+
